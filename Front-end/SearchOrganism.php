ccb13de2afc63001795ce63311adf32d983c6e45
rchapman2022 <rchapman@unomaha.edu>
2022-04-03 01:02:35 -0500

    Created PHP SQL Search functionality

--- Front-end/SearchOrganism.php
+++ Front-end/SearchOrganism.php
@@ -15,9 +15,24 @@ $connect = mysqli_connect($server,$username,"",$database);
 
 $orgname = $_POST['OrgName'];
 
-$query = "SELECT * FROM Organism WHERE orgName = \'$orgname\';"
+$query = "SELECT lineage, organism_rank, organism_name, t.taxID, ncbi_tax_link, refSeq_accession, 
+            ncbi_refseq_link, assembly_link, article_title, article_link FROM Organism AS o JOIN Taxonomy AS t 
+            ON o.taxID = t.taxID JOIN RefSeqEntry AS r ON o.organismID = r.organismID JOIN OrganismPubMedLink AS opl 
+            ON o.organismID = opl.organismID JOIN PubMedEntries AS p ON p.pubmedID = opl.pubmedID WHERE organism_name = \'$orgname\';";
 
+$result = mysqli_query($connect, $query);
 
-echo json_encode($results);
+if ($result = mysqli_query($connect, $query)) {
+    $row = mysqli_fetchrow($result)
+
+    $data = array('status' => "success", 'lineage' => row[0], 'rank' => row[1], 'orgName' => row[2], 'taxID' => row[3], 
+    'taxLink' => row[4], 'assembly' => row[5], 'refSeqLink' => row[6], 'downloadLink' => row[7],
+    'title' => row[8], 'articleLink' => row[9]);
+    
+    echo json_encode($arr);
+}
+else {
+    echo '{"status":"error"}';
+}
 
 ?>
