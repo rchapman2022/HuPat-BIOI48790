cfae62e08fe389ceaf5a6616a5f6c096f6bfd8bd
rchapman <rchapman@ist-odin.UNOMAHA.EDU>
2022-04-05 14:30:41 -0500

    Fixed unknown organism action and made data form initially hidden

--- Front-end/SearchOrganism.php
+++ Front-end/SearchOrganism.php
@@ -29,25 +29,24 @@ $query = "SELECT lineage, organism_rank, organism_name, t.taxID, ncbi_tax_link, 
             ON o.taxID = t.taxID JOIN RefSeqEntry AS r ON o.organismID = r.organismID JOIN OrganismPubMedLink AS opl 
             ON o.organismID = opl.organismID JOIN PubMedEntries AS p ON p.pubmedID = opl.pubmedID WHERE organism_name = '$orgname';";
 
-#echo $query;
 
 $result = mysqli_query($connect, $query);
 
-#echo "Ran query";
 
 if ($result = mysqli_query($connect, $query)) {
-
-    #echo "Hit query success";
-
-    $row = mysqli_fetch_row($result);
-
-    #echo $row;
-
-    $data = array('status' => "success", 'lineage' => "$row[0]", 'rank' => "$row[1]", 'orgName' => "$row[2]", 'taxID' => "$row[3]", 
-    'taxLink' => "$row[4]", 'assembly' => "$row[5]", 'refSeqLink' => "$row[6]", 'downloadLink' => "$row[7]",
-    'title' => "$row[8]", 'articleLink' => "$row[9]");
     
-    echo json_encode($data, JSON_FORCE_OBJECT);
+    $row = mysqli_fetch_row($result);
+    if ($row['0'] == "") {
+	    echo '{"status":"error"}';
+    }
+    else {
+   
+        $data = array('status' => "success", 'lineage' => "$row[0]", 'rank' => "$row[1]", 'orgName' => "$row[2]", 'taxID' => "$row[3]", 
+        'taxLink' => "$row[4]", 'assembly' => "$row[5]", 'refSeqLink' => "$row[6]", 'downloadLink' => "$row[7]",
+        'title' => "$row[8]", 'articleLink' => "$row[9]");
+    
+        echo json_encode($data, JSON_FORCE_OBJECT);
+    }
 }
 else {
     echo '{"status":"error"}';
